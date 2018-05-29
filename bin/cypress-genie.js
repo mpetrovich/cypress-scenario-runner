7560bfa73f265306674db194b3f850bb487e0e4d
Mike Petrovich <michael.c.petrovich@gmail.com>
2018-05-29 01:25:03 -0400

    Use hard-coded path instead of a dynamic lookup

--- bin/cypress-genie.js
+++ bin/cypress-genie.js
@@ -2,12 +2,11 @@
 
 const mergedirs = require('merge-dirs').default;
 const exec = require('child_process').exec;
-const { getInstalledPathSync } = require('get-installed-path');
 
 const command = process.argv[2];
 const path = require('path');
 const pwd = path.resolve();
-const src = path.resolve(getInstalledPathSync('cypress-genie'), 'cypress');
+const src = path.resolve('node_modules', 'cypress-genie', 'cypress');
 const dest = path.resolve(pwd, 'cypress');
 
 if (command === 'install') {

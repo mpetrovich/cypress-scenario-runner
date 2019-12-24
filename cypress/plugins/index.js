bff35d6375113b1468b2da1714377458bca3ba6b
Mike Petrovich <michael.c.petrovich@gmail.com>
2019-12-24 01:21:22 +0100

    Change custom attributes used in tests
    Made consistent for clarity.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -3,9 +3,9 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 module.exports = (on, config) => {
 	on('file:preprocessor', cucumber())
 
-	config.elementAttr = 'test-elem'
-	config.elementValueAttr = 'test-value'
-	config.elementOptionsAttr = 'test-options'
+	config.elementAttr = 't-element'
+	config.elementValueAttr = 't-value'
+	config.elementOptionsAttr = 't-options'
 	config.elementOptions = JSON.stringify({ force: false })
 	config.defaultCommandWait = 0
 
